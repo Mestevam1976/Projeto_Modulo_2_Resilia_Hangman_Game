03bf9e43425068fc75a619790cba8185c0f33975
Mestevam1976 <marcio.estevam@yahoo.com>
2021-12-13 21:20:31 -0300

    Novo

--- files/main.py
+++
@@ -1,6 +0,0 @@
-import functions
-
-functions.limpa_tela()
-functions.imprime_mensagem_abertura()
-functions.numero_jogadores()
-functions.modo_de_jogo()
--- files/messages.py
+++ files/messages.py
@@ -1,11 +1,17 @@
+<<<<<<< HEAD
 ################################## CATEGORIA DE PALAVRAS DO TEMA GERAL #########################################################
 
 geral_faceis = [
     'teste', 'apice', 'animo', 'comum', 'graca', 'graxa', 'terra', 'corda', 'carro', 'visor', 'tampa', 'vidro',
+=======
+geral_faceis = [ # 60 palavras de 6 letras cada
+    'teste', 'apice', 'animo', 'comum', 'graca', 'graxa', 'terra', 'corda', 'carro', 'visor', 'tampa', 'vidro', 
+>>>>>>> parent of e5df7a5 (Adequações gerais - Equipe)
     'barra', 'ganso', 'manso', 'senso', 'algoz', 'nobre', 'exito', 'sagaz', 'inato', 'ideia', 'honra', 'poder',
     'sutil', 'mutua', 'afeto', 'mexer', 'aquem', 'ardil', 'corja', 'prole', 'digno', 'crivo', 'culto', 'mundo',
     'brado', 'gleba', 'tenaz', 'regra', 'saude', 'viril', 'usura', 'clava', 'pifio', 'feliz', 'heroi', 'coisa',
     'plato', 'alibi', 'fluxo', 'senil', 'lugar', 'ligar', 'crise', 'grato', 'vital', 'valor', 'olhar', 'fator',
+<<<<<<< HEAD
 ]
 
 geral_medianas = [
@@ -221,53 +227,56 @@ tech_faceis = ['float', 'dados', 'login', 'array', 'alias', 'chave', 'ascii', 'c
 
 tech_medianas = ['sensitive', 'atributos', 'agregacao', 'abstracao', 'variaveis', 'repeticao',
                  'algoritmo', 'protocolo', 'interface', 'parametro', 'benchmark'
+=======
+] 
 
-                 ]
+geral_medianas = [ # 63 palavras de 8 letras cada
+    'inerente', 'peculiar', 'denegrir', 'respeito', 'genocida', 'anuencia', 'deferido', 'prudente', 'equidade',
+    'pandemia', 'iminente', 'invasivo', 'essencia', 'desgraca', 'alienado', 'misogino', 'eminente', 'extensao',
+    'abstrato', 'empirico', 'premissa', 'conceito', 'ardiloso', 'reiterar', 'ascensao', 'passivel', 'prodigio',
+    'devaneio', 'talarico', 'lascivia', 'conserto', 'modestia', 'apologia', 'analogia', 'insercao', 'ativista',
+    'inospito', 'monotono', 'respaldo', 'remissao', 'sinonimo', 'mediocre', 'concerne', 'rapariga', 'despeito',
+    'gratidao', 'alicerce', 'retorica', 'destarte', 'rechacar', 'fomentar', 'proceder', 'demagogo', 'primazia',
+    'metodico', 'consiste', 'distinto', 'abnegado', 'criterio', 'desfecho', 'sucumbir', 'historia', 'elegivel',
+]
 
-tech_dificeis = [
-    'emulador', 'nickname', 'downtime', 'intranet', 'download', 'wireless', 'password', 'keyboard',
-    'freeware', 'terabyte', 'megabyte', 'kilobyte', 'booleano', 'controle', 'entidade', 'programa',
-    'variavel', ]
+geral_dificeis = [
+    'paralelepipedo', 'empoderamento', 'delineamento', 'caleidoscopio', 'polinesio', 'polifonico',
+]
+
+categoria_geral = [geral_faceis, geral_medianas, geral_faceis]
+
+
+tech_faceis = ['byte', 'bit', 'git', 'put', 'add']
+
+tech_medianas = ['append', 'enumerate', 'while', 'atribuicao', 'repeticao', 'condicional', 'flutuante']
+>>>>>>> parent of e5df7a5 (Adequações gerais - Equipe)
+
+tech_dificeis = ['engenharia_de_dados', 'data_wharehouse', 'business_intelligence', 'data_mining', 'data_science', 'artificial_intelligence',
+'machine_learning','deep_learning']
 
 categoria_tech = [tech_faceis, tech_medianas, tech_dificeis]
 
-tech_dicionario_dificeis = {
-
-    'emulador': 'DICA: imitar: Programa que simula uma plataforma computacional em outra, como, por exemplo, os que permitem que o computador rode jogos de consoles.',
-    'nickname': 'DICA: apelido : Expressão inglesa que significa apelido.',
-    'downtime': 'DICA: tempo de espera : É o tempo que o sistema não está operacional.',
-    'intranet': 'DICA: rede de computadores privada',
-    'download': 'DICA: baixar : Baixar dados da internet para si.',
-    'wireless': 'DICA: Sem fio : comunicação sem fio.',
-    'password': 'DICA: código de acesso.',
-    'keyboard': 'DICA: quadro de teclas : Teclado de computador.',
-    'freeware': 'DICA: software gratuito : Programa gratuito que baixamos da internet.',
-    'terabyte': 'DICA: unidade de medida de dispositivos de armazenamento, igual a um trilhão de bytes',
-    'megabyte': 'DICA: unidade de medida de capacidade de memória que vale 1.084.574 bytes',
-    'kilobyte': 'DICA: unidade de medida que é composto por 1024 bytes.',
-    'booleano': 'DICA: é um tipo de dado que possui dois valores, que podem ser considerados como 0 ou 1, falso ou verdadeiro.',
-    'controle': 'DICA: função que estabelece o curso das operações ou do sistema de processamento de dados',
-    'entidade': 'DICA: pode ser concreta ou abstrata, incluindo associações, que guardarão informações no banco de dados',
-    'programa': 'DICA: é a denominação que se dá a um conjunto organizado de instruções em linguagem natural ou codificada,',
-    'variavel': 'DICA: é um espaço na memória do computador destinado a um dado que é alterado durante a execução do algoritmo.',
+tech_dicionario = {
+'engenharia de dados':' DICA: É a disciplina focada nos apectos como a identificação das fontes de dados, coletânea e,'
+'\nno armazenamento dos dados.',
+'data wharehouse' :' DICA: É um repositório central de informações que pode ser usado para analisar e tomar decisões mais informadas.',
+'business intelligence' :'DICA: É a disciplina de analisar e transformar dados para extrair valiosos insights de negócios para permitir'
+'\na tomada de decisões. Hoje, o BI é normalmente usado para se referir a análises descritivas e relatórios.',
+'data mining' : 'DICA: É um processo de extração e descoberta de padrões em grandes conjuntos de dados envolvendo métodos na interseção'
+'\nde aprendizado de máquina, estatísticas e sistemas de banco de dados.',
+'data science' : 'DICA: É a disciplina de aplicação de técnicas analíticas avançadas para extrair informações valiosas de dados para a'
+'\ntomada de decisões de negócios e planejamento estratégico.',
+'artificial intelligence' : 'DICA: Refere-se à capacidade de uma máquina de imitar as capacidades da mente humana, como aprender com'
+'\nexemplos e experiências, reconhecer objetos, compreender e responder à linguagem, tomar decisões e resolver problemas.',
+'machine learning': 'DICA: É um subconjunto da disciplina de inteligência artificial (IA) que fornece aos sistemas a capacidade'
+'\nde aprender e melhorar automaticamente com a experiência, sem ser explicitamente programado.',
+'deep learning' : 'DICA: É uma técnica que se enquadra na disciplina de aprendizado de máquina. É baseado em redes neurais artificiais'
+'\ninspiradas na estrutura do cérebro humano.',
 }
 
-tech_dicionario_medianas = {
-    'sensitive': 'DICA: significa que caracteres em caixa alta e em caixa baixa são tratados de modo diferente',
-    'atributos': 'DICA: são os elementos que definem a estrutura de uma classe.',
-    'agregacao': 'DICA: é um tipo especial de associação que tenta demonstrar que as informações de um objeto-todo precisam'
-    'ser complementadas pelas informações contidas em um (ou mais) objetos-parte.',
-    'abstracao': 'DICA: a abstração consiste em conseguir identificar um item do mundo real e transformá-lo em uma classe,'
-    'preocupando-se apenas com suas principais características e ações.',
-    'variaveis': 'DICA: é uma posição alocada na memória RAM para armazenar um dado.',
-    'repeticao': 'DICA: é uma estrutura que permite executar mais de uma vez o mesmo comando ou conjunto de comandos',
-    'algoritmo': 'DICA: sequência finita de regras, raciocínios ou operações',
-    'protocolo': 'DICA: é uma convenção que controla e possibilita uma conexão, comunicação, transferência de dados',
-    'interface': 'DICA: é o contrato entre a classe e o mundo exterior.',
-    'parametro': 'DICA: é um valor, proveniente de uma variável ou de uma expressão mais complexa,',
-    'benchmark': 'DICA: é o ato de executar um programa de computador',
-}
 
+<<<<<<< HEAD
 tech_dicionario_faceis = {
     'float': 'DICA: armazena valores numéricos com ponto flutuante com uma precisão simples.',
     'dados': 'DICA: é a matéria prima da informação, ou seja, é a informação não tratada.',
@@ -284,3 +293,6 @@ tech_dicionario_faceis = {
     'vetor': 'DICA: é uma variável que armazena várias variáveis do mesmo tipo.',
     'while': 'DICA: é uma instrução de fluxo de controle que permite que o código seja executado repetidamente',
 }
+=======
+
+>>>>>>> parent of e5df7a5 (Adequações gerais - Equipe)
